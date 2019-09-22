d365c9db65d8fbfc333b56f4a65292a9ddebee6c
Sofie <sofieljungcrantz@hotmail.com>
2019-09-22 10:47:43 +0200

    Edits

--- standardSida.js
+++ standardSida.js
@@ -1,17 +1,31 @@
 function showAbout() {
   show("About", "Hej jag heter mats");
+
+  document.getElementById("about").style.display="block";
+    document.getElementById("about1").style.display="block";
+      document.getElementById("about2").style.display="block";
+
 }
 
 function showContact() {
   show("Contact", "1234567890+0987<br>654321234567890987654323456789");
+  document.getElementById("about").style.display="none";
+  document.getElementById("about1").style.display="none";
+  document.getElementById("about2").style.display="none";
 }
 
 function showEvent() {
   show("Events", "WOewoowooowowowowowowowoww");
+  document.getElementById("about").style.display="none";
+  document.getElementById("about1").style.display="none";
+  document.getElementById("about2").style.display="none";
 }
 
 function showProducts() {
   show("Products", "ÖLÖlölÖLöölölöLölöLöLölÖLö");
+  document.getElementById("about").style.display="none";
+  document.getElementById("about1").style.display="none";
+  document.getElementById("about2").style.display="none";
 }
 
 function show(title, text) {
